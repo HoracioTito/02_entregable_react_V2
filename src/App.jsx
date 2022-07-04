2c9bc017e15919a0cd7d045c90d98eb9650f46bf
Ing Choque Horacio <hchoque@hotmail.com>
2022-07-04 19:37:02 -0300

    Entregable 2  Weather API

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import  './css/style.css'
+import ApliClime from './component/ApliClime'
+
+function App() {
+  
+
+  return (
+    <div className='content' >
+      <ApliClime />
+    </div>
+  )
+}
+
+export default App
---
+++ src/component/ApliClime.jsx
@@ -0,0 +1,97 @@
+import axios from 'axios';
+import React, { useState } from 'react';
+import { useEffect } from 'react';
+
+const ApliClime = () => {
+    /* key : 99fff5c4d9d073d92a9c4d9ebb6f8448 */
+    const keyApi ="99fff5c4d9d073d92a9c4d9ebb6f8448 "
+
+    /* Init variables infoClime , Scale */
+    let [infoClima , setInfoClima] = useState({})
+    /* 0=kelvin  1=celsius 2=Farnaeith */
+    let [scaleValue , setScaleValue] = useState(0)
+    let [newTemperature ,setNewTemperature] = useState(0)
+
+
+
+       useEffect(() => {
+            
+            /* Funcion llamada desde geolocation */
+           const success = pos => {
+                /* Coordenadas geolocation */
+               let latitude = pos.coords.latitude;
+               let longitude = pos.coords.longitude;
+
+               axios.get(`https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${keyApi}`)
+               .then(res => setInfoClima(res.data))
+
+                console.log(infoClima)
+
+           }
+
+            /* Geolocalizacion */
+           navigator.geolocation.getCurrentPosition(success)  
+
+        }, [])
+
+
+    /** Change scale temperature */
+  
+
+    const changeScale=()=>{
+
+        /* Grade Kelvin */
+        let gradeKelvin = infoClima.main?.temp
+
+        /* Cambio de escala 0 , 1, 2 */
+        setScaleValue(scaleValue + 1)
+        
+        if (scaleValue > 2){
+            /* Puesta a cero */
+            setScaleValue( scaleValue = 0 )
+        }   
+ 
+        /* Change Temperature */
+        if(scaleValue == 0 ) {
+            /* Grade Celcius  */
+            setNewTemperature( (Math.trunc ((gradeKelvin - 273.15)*100)/100) + "  ºC")  
+         }else if ( scaleValue === 1){
+             /* Grade  Fahrenheit */
+            setNewTemperature((Math.trunc (((gradeKelvin - 273.15) * 9/5 + 32)*100)/100) +"  ºF")
+        }else{
+            setNewTemperature(gradeKelvin +"  ºK")
+        }
+
+    }
+
+
+
+    return (
+        <div className='card-weather'>
+            <h1 className='title'>Weather</h1>
+            
+            <h2>Pais : {infoClima.sys?.country} </h2>
+            <h3> {infoClima.name} </h3>
+
+            <div className='clime'>
+             <div><img src={`http://openweathermap.org/img/wn/${infoClima.weather?.[0].icon}@2x.png`} /></div>
+             <div><h3>{infoClima.weather?.[0].description}</h3></div>
+            </div>
+ 
+            <h2>Temperature</h2>
+            <p>{newTemperature} </p> 
+            <h2>Winter Speed</h2>
+            <p>{infoClima.wind?.speed} Meter/Sec</p>
+          
+   
+            <p className='p-border'>Cloud : {infoClima.clouds?.all} %</p>
+            <p className='p-border'>Presion : {infoClima?.main?.pressure} mb</p>
+            
+            <button onClick={changeScale} className='btn-temperature' >Degrees</button>
+            <div><small>Change:	Kelvin-Celsius-Fahrenheit</small></div>
+            {/* */}
+        </div>
+    );
+};
+
+export default ApliClime;
